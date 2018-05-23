a6e4c2f901e7a5225ef955bb315d26617f275744
bvduong <duongbv@atmarkcafe.org>
2018-05-23 15:15:13 +0700

    Show detail product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
-	def index 
-		@products = Product.includes(:category).where(pushlished: true)
-	end
+  def index 
+    @products = Product.includes(:category).where(pushlished: true)
+  end
+  def show 
+    @product = Product.find(params[:id])
+  end
 end
