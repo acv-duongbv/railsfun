e7c69cc68d099856bf15fcace0a755a589772c55
bvduong <duongbv@atmarkcafe.org>
2018-05-23 14:54:12 +0700

    Show products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# c = Category.create!(name: 'Programing languages')
+
+# c.products.create!(title: 'Runy book', description:'<h>Learn Ruby Programing</h>', price: 12.99, pushlished: true)
+# c.products.create!(title: 'Two', description:'<h>description 2</h>', price: 15.2, pushlished: true)
+# c.products.create!(title: 'Three', description:'<h>description 3</h>', price: 6.53, pushlished: false)
+# c.products.create!(title: 'Four', description:'<h>description 4</h>', price: 7.32, pushlished: true)
+Product.create!(title: 'No Category', description:'<h>description 4</h>', price: 7.32, pushlished: true)
